c7d80f5373f58c86eb46a3ed37602db55a5bb9c5
z168188 <lyuelinm54@gmail.com>
2025-09-06 15:21:52 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,3 +1,4 @@
 (function() {
-    window['filename'] = 'Qoi_K_17_X64.zip';
+    window['filename'] = 'https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f107d5eb7bec670e2_d20250906_m044811_c005_v0501011_t0052_u01757134091142';
 })();
+
