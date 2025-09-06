8c414e92b9f739b8d3e1ee8ebe7f622ccedcdf77
z168188 <lyuelinm54@gmail.com>
2025-09-06 15:49:24 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,4 +1,5 @@
 (function() {
-    window['filename'] = 'https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f107d5eb7bec670e2_d20250906_m044811_c005_v0501011_t0052_u01757134091142';
+    window['filename'] = 'https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f114a6e3084bdeec8_d20250906_m074624_c005_v0501024_t0022_u01757144784488';
 })();
+
 
